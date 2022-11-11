733f519186e97a86e84d64471e0e3bfc967868cb
Dimaster <gordeev.dmitriy@physics.msu.ru>
2022-11-11 17:22:28 +0300

    fixing all errors

--- src/main/ThreeExample.js
+++ src/main/ThreeExample.js
@@ -1,4 +1,6 @@
 import * as THREE from 'three';
+import {DragControls} from 'three/addons/controls/DragControls.js';
+
 
 
 export class ThreeExample {
@@ -6,10 +8,19 @@ export class ThreeExample {
         this.setupFrameCallback();
         this.canvas = document.getElementById('c');
         this.scale();
+
+        this.mouse = new THREE.Vector2();
+        this.raycaster = new THREE.Raycaster();
+
+        this.enableSelection = false;
+
+        // document.addEventListener('click', onClick);
+        // window.addEventListener('keydown', onKeyDown, true);
+        // window.addEventListener('keyup', onKeyUp, true);
     }
 
     setupFrameCallback() {
-        window.myRequestAnimFrameCall = function () {
+        window.customRequestAnimationFrame = function () {
             return window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame || window.oRequestAnimationFrame || window.msRequestAnimationFrame || function (a) {
                 window.setTimeout(a, 1E3 / 60)
             }
@@ -20,14 +31,13 @@ export class ThreeExample {
         this.dpr = window.devicePixelRatio;
         this.cw = window.innerWidth;
         this.ch = window.innerHeight;
-        // this.canvas.width = this.cw * this.dpr;
-        // this.canvas.height = this.ch * this.dpr;
     }
 
 
-
     animateScene(cube) {
-        window.myRequestAnimFrameCall(() => {this.animateScene(cube)});
+        window.customRequestAnimationFrame(() => {
+            this.animateScene(cube)
+        });
 
         cube.rotation.y += 0.02;
         cube.rotation.x += 0.01;
@@ -37,9 +47,9 @@ export class ThreeExample {
 
     startScene(cube) {
         let canvas = this.canvas;
-        this.render = new THREE.WebGLRenderer({canvas});
-        this.render.setClearColor("#3a3535");
-        this.render.setSize(this.cw, this.ch);
+        this.renderer = new THREE.WebGLRenderer({canvas});
+        this.renderer.setClearColor("#3a3535");
+        this.renderer.setSize(this.cw, this.ch);
 
         this.scene = new THREE.Scene();
         let aspect = this.canvas.width / this.canvas.height;
@@ -56,29 +66,54 @@ export class ThreeExample {
 
     createCube() {
         let cubeMaterials = [
-            new THREE.MeshBasicMaterial({color:0x2173fd}),
-            new THREE.MeshBasicMaterial({color:0xd5d918}),
-            new THREE.MeshBasicMaterial({color:0xd2dbeb}),
-            new THREE.MeshBasicMaterial({color:0xa3a3c6}),
-            new THREE.MeshBasicMaterial({color:0xfe6b9f}),
-            new THREE.MeshBasicMaterial({color:0x856af9})
+            new THREE.MeshBasicMaterial({color: 0x2173fd}),
+            new THREE.MeshBasicMaterial({color: 0xd5d918}),
+            new THREE.MeshBasicMaterial({color: 0xd2dbeb}),
+            new THREE.MeshBasicMaterial({color: 0xa3a3c6}),
+            new THREE.MeshBasicMaterial({color: 0xfe6b9f}),
+            new THREE.MeshBasicMaterial({color: 0x856af9})
         ];
 
         let cubeGeometry = new THREE.BoxGeometry(2, 2, 2);
-        let cube = new THREE.Mesh(cubeGeometry, cubeMaterials);
-        return cube;
+        this.cube = new THREE.Mesh(cubeGeometry, cubeMaterials);
     }
 
 
     renderScene() {
-        this.render.render(this.scene, this.camera);
+        this.renderer.render(this.scene, this.camera);
     }
 
     draw() {
-        let cube = this.createCube();
-        this.startScene(cube);
-        this.animateScene(cube);
+        this.createCube();
+        this.startScene(this.cube);
+
+        this.controls = new DragControls([this.cube], this.camera, this.renderer.domElement);
+        this.controls.addEventListener('drag', () => {this.renderScene()});
+
+        let thisref = this;
+        document.addEventListener('click', () => {
+            const draggableObjects = thisref.controls.getObjects();
+            console.log("draggableObjects = " + draggableObjects);
+
+            thisref.raycaster.setFromCamera( thisref.mouse, thisref.camera );
+            const intersections = thisref.raycaster.intersectObjects([thisref.cube], true);
+            console.log("intersections = " + intersections);
+        });
+
+        window.addEventListener('keydown', () => {
+            console.log("keyDown");
+        }, true);
+
+        window.addEventListener('keyup', () => {
+            console.log("keyUp");
+        }, true);
+
+        this.animateScene(this.cube);
         this.renderScene();
     }
+
+
+
+
 
 }
