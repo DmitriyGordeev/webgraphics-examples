1cf92ab6a4fe79ee4a931ae3f7751c4435d64c38
Dimaster <gordeev.dmitriy@physics.msu.ru>
2022-11-21 17:08:45 +0300

    stable fluid

--- src/main/liquid/stable_fluid2.glsl
+++ src/main/liquid/stable_fluid2.glsl
@@ -1,5 +1,5 @@
 
-#define ROT_NUM 7
+#define ROT_NUM 5
 
 const float ang = 2.0*3.1415926535 / float(ROT_NUM);
 mat2 m = mat2(cos(ang),sin(ang),-sin(ang),cos(ang));
@@ -27,9 +27,9 @@ float getRot(vec2 offset, vec2 b) {
 
         vec4 prevTexel = texture(iChannel0, texelCoord);
 
-        vec2 picker = p.yx * vec2(0.01, 1.1);
+        vec2 picker = p.yx * vec2(1.0, 1.0);
 
-        rot += 0.1 * dot(prevTexel.xy, picker) * dot(prevTexel.xy, picker);
+        rot += 0.8 * dot(prevTexel.xy, picker) * dot(prevTexel.xy, picker);
 
         p = m * p;
 
@@ -47,21 +47,21 @@ void mainImage( out vec4 fragColor, in vec2 fragCoord )
 
     float r = randS(vec2(float(iTime) / iResolution.x, 0.5 / iResolution.y)).x;
 
-    vec2 b = 2.0 * vec2(cos(ang * r), sin(ang * r));
+    vec2 b = vec2(cos(ang * r), sin(ang * r));
 
     vec2 v = vec2(0);
 
 
 
 
-    for (int i = 0; i < 5; i++) {
+    for (int i = 0; i < 3; i++) {
 
         vec2 p = b;
 
         for(int j = 0; j < ROT_NUM; j++) {
 
 
-            v += p.yx * getRot(pos + p, 2.5 * b);
+            v += p.yx * 1.0 * getRot(pos + p, 1.1 * b);
 
 
             p = m * p;
