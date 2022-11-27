e62dafc7b92b7061c81f726ef2a617cdbf78c280
Dimaster <gordeev.dmitriy@physics.msu.ru>
2022-11-27 16:36:55 +0300

    encoded vel didn't work

--- src/main/custom_liquid/encoded_vel.glsl
+++ src/main/custom_liquid/encoded_vel.glsl
@@ -2,7 +2,7 @@ vec2 figureCenter = vec2(0.5);
 
 
 // coeffs
-float offset = 0.1;
+float offset = 0.001;
 
 
 const float PI = 2.0 * 3.1415926535;
@@ -28,7 +28,7 @@ bool drawCircle(vec2 center, vec2 uv, float radius) {
 
 // color to impulse
 vec3 cti(vec4 color) {
-    return vec3(color.r, 2.0 * color.g - 1.0, 2.0 * color.b  - 1.0);
+    return vec3(color.r, 2.0 * color.g - 1.0, 2.0 * color.b - 1.0);
 }
 
 
@@ -56,61 +56,73 @@ void mainImage(out vec4 fragColor, in vec2 fragCoord)
     // 1. Mass outcome
     float newM0 = I0.r * (sqrt2 - sqrt(I0.g * I0.g + I0.b * I0.b)) / sqrt2;
 
+
     // 2. Mass income
     int count = 0;
     if (I1.g > 0.0) {
-        newM0 += I1.r * I1.g;
+        // newM0 += I1.r * I1.g;
+        newM0 += I1.r * sqrt(I1.g * I1.g + I1.b * I1.b) / sqrt2;
         count++;
     }
 
     if (I2.b < 0.0) {
-        newM0 += I2.r * abs(I2.b);      // todo: abs -> -= ?
+        newM0 += I2.r * sqrt(I2.g * I2.g + I2.b * I2.b) / sqrt2;      // todo: abs -> -= ?
         count++;
     }
 
     if (I3.g < 0.0) {
-        newM0 += I3.r * abs(I3.g);      // todo: abs -> -= ?
+        newM0 += I3.r * sqrt(I3.g * I3.g + I3.b * I3.b) / sqrt2;    // todo: abs -> -= ?
         count++;
     }
 
     if (I4.b > 0.0) {
-        newM0 += I4.r * I4.b;
+        newM0 += I4.r * sqrt(I4.g * I4.g + I4.b * I4.b) / sqrt2;
         count++;
     }
-    newM0 /= float(count);
 
 
 
-    // 3. Xvel change
-    float newVg0 = I0.g;
-    if (I1.g > 0.0) {
-        newVg0 += I1.g * I1.r / (I1.r + I0.r);
+    if (newM0 != 0.0) {
+        // 3. Xvel change
+        float newVg0 = I0.g;
+        if (I1.g > 0.0) {
+            newVg0 += I1.g * I1.r / (I1.r + I0.r);
+        }
+        if (I3.g < 0.0) {
+            newVg0 += I3.g * I3.r / (I3.r + I0.r);
+        }
+
+
+        // 4. Yvel change
+        float newVb0 = I0.b;
+        if (I2.b < 0.0) {
+            // newVb0 += I2.b * I2.r / (I2.r + I0.r);
+        }
+        if (I4.b > 0.0) {
+            // newVb0 += I4.b * I4.r / (I4.r + I0.r);
+        }
+
+        fragColor = itc(vec3(newM0, newVg0, newVb0));
     }
-    if (I3.g < 0.0) {
-        newVg0 += I3.g * I3.r / (I3.r + I0.r);
+    else {
+        fragColor = itc(vec3(0.0, 0.0, 0.0));
     }
 
-    // 4. Yvel change
-    float newVb0 = I0.b;
-    if (I2.b < 0.0) {
-        newVb0 += I2.b * I2.r / (I2.r + I0.r);
-    }
-    if (I4.b > 0.0) {
-        newVb0 += I4.b * I4.r / (I4.r + I0.r);
-    }
-
-
-    fragColor = itc(vec3(newM0, newVg0, newVb0));
 
 
 
     // Initial figure
     if (iFrame < 2) {
         if (drawCircle(figureCenter, uv, 0.1)) {
-            fragColor = vec4(1.0, 0.0, 0.0, 1.0);
+
+            fragColor = itc(vec3(1.0, 0.01, 0.0));
+
         }
         else {
-            fragColor = vec4(vec3(0.0), 1.0);
+            // zero mass and zero speed (no liquid there)
+            // fragColor = vec4(0.0, 0.5, 0.5, 1.0);
+
+            fragColor = itc(vec3(0.0));
         }
     }
 
