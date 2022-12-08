96ffe5a8f666d1a6b87f173e0a177093867be699
Dimaster <gordeev.dmitriy@physics.msu.ru>
2022-12-08 12:05:09 +0300

    found a cause of error

--- src/main/custom_liquid/encoded_vel_round.glsl
+++ src/main/custom_liquid/encoded_vel_round.glsl
@@ -1,7 +1,7 @@
 vec2 figureCenter = vec2(0.5);
 
 
-const float offsetPx = 10.0;
+const float offsetPx = 3.0;
 
 
 const float PI = 3.1415926535;
@@ -47,10 +47,16 @@ const vec2 e1 = vec2(offsetPx, 0.0);
 const vec2 e2 = vec2(floor(offsetPx * cos(a2)), floor(offsetPx * sin(a2)));
 const vec2 e3 = vec2(0.0, offsetPx);
 const vec2 e4 = vec2(floor(offsetPx * cos(a4)), floor(offsetPx * sin(a4)));
-const vec2 e5 = vec2(-offsetPx, 0.0);
-const vec2 e6 = vec2(floor(offsetPx * cos(a6)), floor(offsetPx * sin(a6)));
-const vec2 e7 = vec2(0.0, -offsetPx);
-const vec2 e8 = vec2(floor(offsetPx * cos(a8)), floor(offsetPx * sin(a8)));
+
+const vec2 e5 = -e1;
+const vec2 e6 = -e2;
+const vec2 e7 = -e3;
+const vec2 e8 = -e4;
+
+//const vec2 e5 = vec2(-offsetPx, 0.0);
+//const vec2 e6 = vec2(floor(offsetPx * cos(a6)), floor(offsetPx * sin(a6)));
+//const vec2 e7 = vec2(0.0, -offsetPx);
+//const vec2 e8 = vec2(floor(offsetPx * cos(a8)), floor(offsetPx * sin(a8)));
 
 
 
@@ -58,25 +64,25 @@ float weightSum(vec2 vel) {
     float outValue = 0.0;
 //    if (dot(vel, e1) > 0.0)
 //        outValue += dot(vel, e1) / offsetPx;
+
+    if (dot(vel, e2) > 0.0)
+        outValue += dot(vel, e2) / offsetPx;
+
+//    if (dot(vel, e3) > 0.0)
+//        outValue += dot(vel, e3) / offsetPx;
 //
-//    if (dot(vel, e2) > 0.0)
-//        outValue += dot(vel, e2) / offsetPx;
-
-    if (dot(vel, e3) > 0.0)
-        outValue += dot(vel, e3) / offsetPx;
-
 //    if (dot(vel, e4) > 0.0)
 //        outValue += dot(vel, e4) / offsetPx;
 //
 //    if (dot(vel, e5) > 0.0)
 //        outValue += dot(vel, e5) / offsetPx;
+
+    if (dot(vel, e6) > 0.0)
+        outValue += dot(vel, e6) / offsetPx;
+
+//    if (dot(vel, e7) > 0.0)
+//        outValue += dot(vel, e7) / offsetPx;
 //
-//    if (dot(vel, e6) > 0.0)
-//        outValue += dot(vel, e6) / offsetPx;
-
-    if (dot(vel, e7) > 0.0)
-        outValue += dot(vel, e7) / offsetPx;
-
 //    if (dot(vel, e8) > 0.0)
 //        outValue += dot(vel, e8) / offsetPx;
     return outValue;
@@ -145,28 +151,28 @@ void mainImage(out vec4 fragColor, in vec2 fragCoord)
     float w08 = 0.0;
 
     if (S > 0.0) {
-//        w01 = dot(vel0, e1) / S;
-//        w02 = dot(vel0, e2) / S;
-        w03 = dot(vel0, e3) / S / offsetPx;
-//        w04 = dot(vel0, e4) / S;
-//        w05 = dot(vel0, e5) / S;
-//        w06 = dot(vel0, e6) / S;
-        w07 = dot(vel0, e7) / S / offsetPx;
-//        w08 = dot(vel0, e8) / S;
+//        w01 = dot(vel0, e1) / S / offsetPx;
+        w02 = dot(vel0, e2) / S / offsetPx;
+//        w03 = dot(vel0, e3) / S / offsetPx;
+//        w04 = dot(vel0, e4) / S / offsetPx;
+//        w05 = dot(vel0, e5) / S / offsetPx;
+        w06 = dot(vel0, e6) / S / offsetPx;
+//        w07 = dot(vel0, e7) / S / offsetPx;
+//        w08 = dot(vel0, e8) / S / offsetPx;
     }
 
 //    if (w01 > 0.0) {
 //        massOutflow += w01 * I0.r;
 //    }
-//
-//    if (w02 > 0.0) {
-//        massOutflow += w02 * I0.r;
-//    }
 
-    if (w03 > 0.0) {
-        massOutflow += w03 * I0.r;
+    if (w02 > 0.0) {
+        massOutflow += w02 * I0.r;
     }
 
+//    if (w03 > 0.0) {
+//        massOutflow += w03 * I0.r;
+//    }
+//
 //    if (w04 > 0.0) {
 //        massOutflow += w04 * I0.r;
 //    }
@@ -174,15 +180,15 @@ void mainImage(out vec4 fragColor, in vec2 fragCoord)
 //    if (w05 > 0.0) {
 //        massOutflow += w05 * I0.r;
 //    }
-//
-//    if (w06 > 0.0) {
-//        massOutflow += w06 * I0.r;
-//    }
 
-    if (w07 > 0.0) {
-        massOutflow += w07 * I0.r;
+    if (w06 > 0.0) {
+        massOutflow += w06 * I0.r;
     }
 
+//    if (w07 > 0.0) {
+//        massOutflow += w07 * I0.r;
+//    }
+//
 //    if (w08 > 0.0) {
 //        massOutflow += w08 * I0.r;
 //    }
@@ -193,28 +199,40 @@ void mainImage(out vec4 fragColor, in vec2 fragCoord)
     float massInflow = 0.0;
 
 
-    // point 1
+//    // point 1
 //    vec2 vel1 = vec2(I1.g, I1.b);
 //    massInflow += getMassInflow(vel1, e1, I1.r);
 //    vec2 newVel10 = getNewVel(vel0, vel1, I0.r, I1.r, e1);
+
+
+    // point 2
+    vec2 vel2 = vec2(I2.g, I2.b);
+    float massInflow20 = getMassInflow(vel2, e2, I2.r);
+    massInflow += massInflow20;
+    vec2 newVel20 = getNewVel(vel0, vel2, I0.r, I2.r, e2);
+    
+    
+    // DEBUG ------ 
+    float sVel2 = weightSum(vel2);
+    float massInflow20_DEBUG = 0.0;
+    if (dot(vel2, -e2) > 0.0) {
+        massInflow20_DEBUG = I2.r * dot(vel2, -e2) / sVel2 / offsetPx;
+    }
+    // DEBUG ------ 
+    
+    
+
+//    // point 3
+//    vec2 vel3 = vec2(I3.g, I3.b);
+//    float massInflow30 = getMassInflow(vel3, e3, I3.r);
+//    massInflow += massInflow30;
+//    vec2 newVel30 = getNewVel(vel0, vel3, I0.r, I3.r, e3);
 //
 //
-//    // point 2
-//    vec2 vel2 = vec2(I2.g, I2.b);
-//    massInflow += getMassInflow(vel2, e2, I2.r);
-//    vec2 newVel20 = getNewVel(vel0, vel2, I0.r, I2.r, e2);
-
-
-    // point 3
-    vec2 vel3 = vec2(I3.g, I3.b);
-    float massInflow03 = getMassInflow(vel3, e3, I3.r);
-    massInflow += massInflow03;
-    vec2 newVel30 = getNewVel(vel0, vel3, I0.r, I3.r, e3);
-
-
 //    // point 4
 //    vec2 vel4 = vec2(I4.g, I4.b);
-//    massInflow += getMassInflow(vel4, e4, I4.r);
+//    float massInflow40 = getMassInflow(vel4, e4, I4.r);
+//    massInflow += massInflow40;
 //    vec2 newVel40 = getNewVel(vel0, vel4, I0.r, I4.r, e4);
 //
 //
@@ -222,40 +240,32 @@ void mainImage(out vec4 fragColor, in vec2 fragCoord)
 //    vec2 vel5 = vec2(I5.g, I5.b);
 //    massInflow += getMassInflow(vel5, e5, I5.r);
 //    vec2 newVel50 = getNewVel(vel0, vel5, I0.r, I5.r, e5);
+
+
+    // point 6
+    vec2 vel6 = vec2(I6.g, I6.b);
+    float massInflow60 = getMassInflow(vel6, e6, I6.r);
+    massInflow += massInflow60;
+    vec2 newVel60 = getNewVel(vel0, vel6, I0.r, I6.r, e6);
+
+//    // point 7
+//    vec2 vel7 = vec2(I7.g, I7.b);
+//    float massInflow70 = getMassInflow(vel7, e7, I7.r);
+//    massInflow += massInflow70;
+//    vec2 newVel70 = getNewVel(vel0, vel7, I0.r, I7.r, e7);
 //
 //
-//    // point 6
-//    vec2 vel6 = vec2(I6.g, I6.b);
-//    massInflow += getMassInflow(vel6, e6, I6.r);
-//    vec2 newVel60 = getNewVel(vel0, vel6, I0.r, I6.r, e6);
-
-
-    // point 7
-    vec2 vel7 = vec2(I7.g, I7.b);
-    massInflow += getMassInflow(vel7, e7, I7.r);
-    vec2 newVel70 = getNewVel(vel0, vel7, I0.r, I7.r, e7);
-
-
-    // ---- DEBUG 7 ----
-    float mw70 = I7.r / (I0.r + I7.r);
-    vec2 deltaVel70 = vel7 - vel0;
-
-    float dir70 = dot(deltaVel70, e7) / offsetPx;
-    vec2 newVel0_DEBUG = vel0;
-
-    if (dir70 < 0.0 && length(vel0) > 0.000001 && length(deltaVel70) > 0.000001) {
-        newVel0_DEBUG.x -= dir70 / length(deltaVel70) * deltaVel70.x * mw70;
-        newVel0_DEBUG.y -= dir70 / length(deltaVel70) * deltaVel70.y * mw70;
-    }
-    // ---- DEBUG 7 ----
-
-
 //    // point 8
 //    vec2 vel8 = vec2(I8.g, I8.b);
-//    massInflow += getMassInflow(vel8, e8, I8.r);
+//    float massInflow80 = getMassInflow(vel8, e8, I8.r);
+//    massInflow += massInflow80;
 //    vec2 newVel80 = getNewVel(vel0, vel8, I0.r, I8.r, e8);
 
+
+
     float newMass = I0.r - massOutflow + massInflow;
+    if (newMass < 0.0)
+        newMass = 0.0;
 
 
     vec2 newVel = vec2(0.0);
@@ -263,27 +273,30 @@ void mainImage(out vec4 fragColor, in vec2 fragCoord)
 //        newVel += newVel10 + newVel20 + newVel30 +
 //                newVel40 + newVel50 + newVel60 + newVel70 + newVel80;
 
-        newVel += newVel30 + newVel70;
+        newVel += newVel20 + newVel60;
     }
-
 
 
     vec4 finalColor = itc(vec3(newMass, newVel.x, newVel.y));
 
-    if (I7.r > 0.0 && newVel0_DEBUG.y > -1.0) {
-        finalColor.r = 1.0;
-        finalColor.g = 1.0;
-        finalColor.b = 1.0;
-    }
+    // TODO : почему масса в центре постепенно стремится к 0.0 ?
 
+//     if (massOutflow > massInflow && massOutflow - massInflow > 0.1) {
+//         finalColor = vec4(1.0);
+//     }
+
+//    if (abs(massInflow20_DEBUG - w06 * I0.r) == 0.0) {
+//        finalColor = vec4(1.0);
+//    }
+    
     fragColor = finalColor;
 
 
     // Initial figure
     if (iFrame < 2) {
-        if (drawBox(figureCenter, uv, 0.3, 0.3)) {
+        if (drawBox(figureCenter, uv, 0.2, 0.3)) {
 
-            fragColor = vec4(1.0, 0.5, -1.0, 1.0);
+            fragColor = itc(vec3(1.0, 0.0, -1.0));
         }
         else {
             // zero mass and zero speed (no liquid there)
