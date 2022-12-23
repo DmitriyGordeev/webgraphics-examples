f8e3c730f5bf3f02390c849a9a7ee6ba1ae8f03a
Dimaster <gordeev.dmitriy@physics.msu.ru>
2022-12-23 12:06:52 +0300

    hiding tooltip

--- src/main/App.jsx
+++ src/main/App.jsx
@@ -13,12 +13,19 @@ import {ThreeShader2Channels} from './ThreeShader2Channels';
 class App extends React.Component {
     constructor(props) {
         super(props);
+
+        this.state = {
+            tooltipHidden: false
+        }
+
         this.anim = null;
     }
 
     componentDidMount() {
         this.anim = new ThreeShader2Channels();
         this.anim.entry();
+
+        // console.log(`[]this.anim.bottleState = ${this.anim.bottleState}`);
 
         // this.anim = new ThreeShadersExample();
         // this.anim.entry();
@@ -28,13 +35,39 @@ class App extends React.Component {
 
         // this.anim = new PureWebGl();
         // this.anim.draw();
+
+        addEventListener('mousedown', (event) => {
+            this.mouseDownHandler(event, this)
+        });
     }
 
+    componentWillUnmount() {
+        // Make sure to remove the DOM listener when the component is unmounted.
+        removeEventListener('mousedown', this.mouseDownHandler);
+    }
+
+    mouseDownHandler(event, componentRef) {
+        componentRef.setState(prevState => {
+            return {...prevState, tooltipHidden: true};
+        });
+
+        // setTimeout(() => {}, 1500);
+    }
 
     render() {
+
         return (
             <div>
                 <canvas id={"c"} />
+
+                <div className={"tooltip-container"}>
+                    <p style={{opacity: 1.0 - this.state.tooltipHidden}}
+                       className={"tooltip"}>Drag the cap to the right</p>
+
+                    <div className={this.state.tooltipHidden ? 'hidden-ball' : 'animated-ball'} />
+                </div>
+
+
             </div>
         );
     }
